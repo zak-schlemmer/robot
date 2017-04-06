ee2e70043e0601258dfffb3ff7c7eb682183f8a4
Zak Schlemmer <zak.schlemmer@gmail.com>
2017-04-06 00:41:55 -0400

    probably working ngrok

--- src/ngrok.sh
+++ src/ngrok.sh
@@ -15,32 +15,8 @@ echo -n "Please enter the ngrok server alias for this project: "
 read server_alias
 
 # create the server alias in the vhost based on project case
-case $project in
-    sfdev )
-        docker exec -t ${project}_web_1 bash -c "sed -i '3i\serveralias '${server_alias}'\;' /etc/apache2/sites-available/storefrontdevelop.robot"
-        docker exec -t nginx_1 bash -c 'sed -i -e "s/develop\.robot\;/develop\.robot '${server_alias}'\;/g" /etc/nginx/nginx.conf'
-        ;;
-    sfdev )
-        docker exec -t ${project}_web_1 bash -c "sed -i '3i\serveralias '${server_alias}'\;' /etc/apache2/sites-available/storefrontedge.robot"
-        docker exec -t nginx_1 bash -c 'sed -i -e "s/edge\.robot\;/edge\.robot '${server_alias}'\;/g" /etc/nginx/nginx.conf'
-        ;;
-    fc )
-        docker exec -t ${project}_web_1 bash -c "sed -i '3i\serveralias '${server_alias}'\;' /etc/apache2/sites-available/flightcontrol.robot"
-        docker exec -t nginx_1 bash -c 'sed -i -e "s/control\.robot\;/control\.robot '${server_alias}'\;/g" /etc/nginx/nginx.conf'
-        ;;
-    designer )
-        docker exec -t ${project}_web_1 bash -c "sed -i '3i\serveralias '${server_alias}'\;' /etc/apache2/sites-available/designer.robot"
-        docker exec -t nginx_1 bash -c 'sed -i -e "s/designer\.robot\;/designer\.robot '${server_alias}'\;/g" /etc/nginx/nginx.conf'
-        ;;
-    tupss_bo )
-        docker exec -t ${project}_web_1 bash -c "sed -i '3i\serveralias '${server_alias}'\;' /etc/apache2/sites-available/tupssbo.robot"
-        docker exec -t nginx_1 bash -c 'sed -i -e "s/bo\.robot\;/bo\.robot '${server_alias}'\;/g" /etc/nginx/nginx.conf'
-        ;;
-    tupss_center )
-        docker exec -t ${project}_web_1 bash -c "sed -i '3i\serveralias '${server_alias}'\;' /etc/apache2/sites-available/tupsscenter.robot"
-        docker exec -t nginx_1 bash -c 'sed -i -e "s/center\.robot\;/center\.robot '${server_alias}'\;/g" /etc/nginx/nginx.conf'
-        ;;
-esac
+docker exec -t ${project}_web_1 bash -c "sed -i '3i\serveralias '${server_alias}'\;' /etc/apache2/sites-available/${project}.robot"
+docker exec -t nginx_1 bash -c 'sed -i -e "s/${project}\.robot\;/${project}\.robot '${server_alias}'\;/g" /etc/nginx/nginx.conf'
 
 # reload apache we modified
 docker exec -t ${project}_web_1 bash -c "/etc/init.d/apache2 reload"
