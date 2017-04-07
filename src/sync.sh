896d712648679beb5693616778263d7800f78022
Zak Schlemmer <zak.schlemmer@gmail.com>
2017-04-07 09:31:23 -0400

    adjust sync commands to use new layout

--- src/sync.sh
+++ src/sync.sh
@@ -14,6 +14,9 @@
 where=`. /etc/robot/src/determine.navigation.sh`
 project=` . /etc/robot/src/determine.project.sh`
 
+# probably need this for most things
+project_folder=`ls -d /etc/robot/projects/*/* | grep $project | sed 's/.*projects\///' | sed "s/\/${project}//"`
+
 # arguments to 'robot sync'
 case $2 in
 
@@ -30,19 +33,19 @@ case $2 in
 
     # restart the auto sync for your location
     restart )
-        docker-sync stop -c /etc/robot/docker-sync/${project}-docker-sync.yml --dir ~/robot.dev/docker-sync"${where}"
+        docker-sync stop -c /etc/robot/projects/$project_folder/$project/docker-sync/docker-compose.yml --dir ~/robot.dev/docker-sync"${where}"
         sleep 2
-        docker-sync start -c /etc/robot/docker-sync/${project}-docker-sync.yml --dir ~/robot.dev/docker-sync"${where}" --daemon
+        docker-sync start -c /etc/robot/projects/$project_folder/$project/docker-sync/docker-compose.yml --dir ~/robot.dev/docker-sync"${where}" --daemon
         ;;
 
     # stop the auto sync for your location
     stop )
-        docker-sync stop -c /etc/robot/docker-sync/${project}-docker-sync.yml --dir ~/robot.dev/docker-sync"${where}"
+        docker-sync stop -c /etc/robot/projects/$project_folder/$project/docker-sync/docker-compose.yml --dir ~/robot.dev/docker-sync"${where}"
         ;;
 
     # start the auto sync for your location
     start )
-        docker-sync start -c /etc/robot/docker-sync/${project}-docker-sync.yml --dir ~/robot.dev/docker-sync"${where}" --daemon
+        docker-sync start -c /etc/robot/projects/$project_folder/$project/docker-sync/docker-compose.yml --dir ~/robot.dev/docker-sync"${where}" --daemon
         ;;
 
     # check if sync is running
