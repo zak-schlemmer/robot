021bca136cb6c6ffb3e25e4e468d917a2bfdcbfd
Zak Schlemmer <zak.schlemmer@gmail.com>
2017-04-08 11:09:51 -0400

    replace old nginx container name with robot-nginx

--- src/ngrok.sh
+++ src/ngrok.sh
@@ -16,7 +16,7 @@ read server_alias
 
 # create the server alias in the vhost based on project case
 docker exec -t ${project}_web_1 bash -c "sed -i '3i\serveralias '${server_alias}'\;' /etc/apache2/sites-available/${project}.robot"
-docker exec -t nginx_1 bash -c 'sed -i -e "s/${project}\.robot\;/${project}\.robot '${server_alias}'\;/g" /etc/nginx/nginx.conf'
+docker exec -t robot_nginx_1 bash -c 'sed -i -e "s/${project}\.robot\;/${project}\.robot '${server_alias}'\;/g" /etc/nginx/nginx.conf'
 
 # reload apache we modified
 docker exec -t ${project}_web_1 bash -c "/etc/init.d/apache2 reload"
